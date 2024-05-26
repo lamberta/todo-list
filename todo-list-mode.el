a5a6d241211deff937280787038d5f7036e54a0a
Billy Lamberta <43051+lamberta@users.noreply.github.com>
2024-05-26 14:41:05 -0700

    Update todo-list-mode with useful parts of gtd-mode.

--- todo-list-mode.el
+++ todo-list-mode.el
@@ -1,13 +1,13 @@
 ;; todo-list-mode.el
-;; Emacs major-mode for highlighting a plain-text, numbered todo list.
+;; Emacs to set your goals while keeping up with life.
 ;;
 ;; Copyright (C) 2009 Billy Lamberta
 ;;
-;; Author: Billy Lamberta <b@lamberta.org>
+;; Author: Billy Lamberta
 ;; Created: Jan 2009
-;; Updated: Jan 2014
-;; Keywords: todo
-;; $Revision: 0.3 $
+;; Updated: May 2024
+;; Keywords: todo, todo-list, gtd, outline
+;; $Revision: 1.0 $
 ;;
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -26,165 +26,237 @@
 ;;
 ;; Description:
 ;;
-;; Major-mode for highlighting a plain-text, numbered todo list.
+;; `todo-list-mode' is a minor mode to quickly capture and navigate your
+;; projects and goals labeled within plain text files. This is my simplified
+;; interpretation of Getting Things Done (David Allen) after 10 years of use.
+;; Here, the six horizons of focus are reduced to two file collections:
+;; 1. active *projects* with attached tasks, and
+;; 2. *goals* that set direction to generate projects.
+;; Headings are available to group these further, as needed.
 ;;
-;; A simple mode that uses a few regexp's to determine the color of
-;; a line based on the assigned priority number. Files are auto-sorted
-;; and saved after a new entry.
+;; Setup:
 ;;
-;; In your .emacs:
-;;   (add-to-list 'load-path "~/my-lisp-path/")
-;;   (autoload 'todo-list-mode "todo-list-mode")
+;; It's important to quickly capture ideas for processing later. Add this to
+;; your `.emacs' with an easy keybinding to open your files:
 ;;
-;; To use: M-x todo-list-mode
+;; (autoload 'todo-list-mode "todo-list-mode" "Set your goals." t)
 ;;
-;; To access it at the push of a button:
-;;   (defun open-todo-list ()
-;;     "Open up a todo list in todo-list-mode."
-;;     (interactive)
-;;     (find-file "~/path-to/todo.txt")
-;;     (todo-list-mode))
-;;   (global-set-key [f12] 'open-todo-list) ;shortcut
+;; (setq todo-list-files
+;;   '(:left "~/todo/projects.txt"
+;;     :right "~/todo/goals.txt"
+;;     :hide-right t))  ; collapse outline
 ;;
-;; The format for a given line is:
-;; [number] [whitespace] [text]
+;; (autoload 'todo-list-toggle "todo-list-mode" "Quickly view your goals." t)
+;; (global-set-key (kbd "s-t") 'todo-list-toggle)  ; macos
 ;;
-;; The priority number can include an optional lowercase letter after it.
-;; A line starting with 'C' is marked as complete.
-;; Hash-style comments are highlighted to the end of the line.
+;; Usage:
 ;;
-;; Example:
-;; 0  Goal: Build a Time Machine
-;; 1a Find a DeLorean
-;; 1b Acquire plutonium #watch out for Libyan terrorists
-;; 2  Read docs on flux capacitors #1.21 gigawatts!
-;; 3  Try not to cause too many problems while traveling
-;; 4  Get bulletproof vest for Doc
-;; C  Learn Johnny B. Goode on guitar
+;; In your text files, create project labels at the beginning of a line and
+;; prefix with '@'.
+;; This mode uses `outline-mode' to collapse and expand entries. Headings start
+;; with '#' and can separate multiple projects, such as:
 ;;
-;; Regexps and faces are defined in this file below. Feel free to add
-;; more or change the colors to suit your own style.
+;; @read-gravitys-rainbow
+;; Buy book
+;; Find a dictionary
 ;;
-;; Navigation:
-;; TAB    Jump between columns
-;; RETURN When cursor at end of line, automatically sort buffer
+;; # Work projects
 ;;
-;;  urgent  not-urgent
-;; |-------|-------|
-;; |   1   |   2   | important
-;; |-------|-------|
-;; |   3   |   4   | not-important
-;; |-------|-------|
+;; @promo-project
+;; Sort paperclips by weight
 
-(setq todo-list-highlight-regexps '(
-   ("^0[a-z]?[\t ].*$" 0 todo-list-zero-face t)  ;0 ...
-   ("^1[a-z]?[\t ].*$" 0 todo-list-one-face t)   ;1 ...
-   ("^2[a-z]?[\t ].*$" 0 todo-list-two-face t)   ;2 ...
-   ("^3[a-z]?[\t ].*$" 0 todo-list-three-face t) ;3 ...
-   ("^4[a-z]?[\t ].*$" 0 todo-list-four-face t)  ;4 ...
-   ("^[Cc][\t ].*$" 0 todo-list-complete-face t) ;task completed
-   ("#.*$" 0 font-lock-comment-face t)))         ;comments
+(require 'outline)
 
-(define-derived-mode todo-list-mode fundamental-mode
-  "todo-list-mode"
-  "Major mode for syntax color highlighting of a numbered todo list."
-  (setq font-lock-defaults '(todo-list-highlight-regexps)))
+(defgroup todo-list nil
+  "Customization group for `todo-list-mode'."
+  :prefix "todo-list-"
+  :group 'editing)
 
-(setq require-final-newline t)
+;; USER SETTINGS
 
-;;
-;; font faces for todo entries
-;;
+(defcustom todo-list-files nil
+  "Plist to set the file paths to open. Uses the following keys:
+  - :left (string): File to open in the left buffer.
+  - :right (string): File to open in the right buffer.
+  - :hide-left (boolean): Whether to collapse the outline headers.
+  - :hide-right (boolean): Whether to collapse the outline headers."
+  :type '(plist :key-type (choice (const :left)
+                                  (const :right)
+                                  (const :hide-left)
+                                  (const :hide-right))
+                :value-type (choice (file :must-match t)
+                                    boolean))
+  :group 'todo-list)
 
-(defface todo-list-zero-face'(
-  (((class color) (background dark)) (:foreground "white" :slant italic))
-  (((class color) (background light)) (:foreground "black" :slant italic))
-  (t (:bold t :italic t)))
-  "Todo-List mode face used for level 0 task."
-  :group 'todo-list-mode-highlighting-faces)
-(defvar todo-list-zero-face 'todo-list-zero-face
-  "Todo-List mode face used for level 0 task.")
+(defcustom todo-list-font-size -2
+  "Relative adjustment of font size across open buffers. [0, 1, -1, ...]"
+  :type 'integer
+  :group 'todo-list)
 
-(defface todo-list-one-face'(
-  (((class color) (background dark))  (:foreground "IndianRed1"))
-  (((class color) (background light)) (:foreground "Red3"))
-  (t (:bold t :italic t)))
-  "Todo-List mode face used for level 1 task."
-  :group 'todo-list-mode-highlighting-faces)
-(defvar todo-list-one-face 'todo-list-one-face
-  "Todo-List mode face used for level 1 task.")
+;; COLORS
 
-(defface todo-list-two-face'(
-  (((class color) (background dark))  (:foreground "CadetBlue2"))
-  (((class color) (background light)) (:foreground "Blue3"))
-  (t (:bold t :italic t)))
-  "Todo-List mode face used for level 2 task."
-  :group 'todo-list-mode-highlighting-faces)
-(defvar todo-list-two-face 'todo-list-two-face
-  "Todo-List mode face used for level 2 task.")
+(defface todo-list-project-face
+  '((t (:foreground "#DFAF8F" :bold t)))  ; zenburn-orange
+  "Face for project labels."
+  :group 'todo-list)
 
-(defface todo-list-three-face'(
-  (((class color) (background dark))  (:foreground "MistyRose2"))
-  (((class color) (background light)) (:foreground "DarkOrange3"))
-  (t (:bold t :italic t)))
-  "Todo-List mode face used for level 3 task."
-  :group 'todo-list-mode-highlighting-faces)
-(defvar todo-list-three-face 'todo-list-three-face
-  "Todo-List mode face used for level 3 task.")
+(defface todo-list-heading-face
+  '((t (:foreground "#8CD0D3")))  ; zenburn-blue
+  "Face for headings between multiple projects."
+  :group 'todo-list)
 
-(defface todo-list-four-face'(
-  (((class color) (background dark))  (:foreground "LightSteelBlue1"))
-  (((class color) (background light)) (:foreground "DodgerBlue2"))
-  (t (:bold t :italic t)))
-  "Todo-List mode face used for level 4 task."
-  :group 'todo-list-mode-highlighting-faces)
-(defvar todo-list-four-face 'todo-list-four-face
-  "Todo-List mode face used for level 4 task.")
+(defface todo-list-comment-face
+  '((t (:foreground "#5F7F5F" :italic t)))  ; zenburn-green-1
+  "Face for comments and subdued text."
+  :group 'todo-list)
 
-(defface todo-list-complete-face'(
-  (((class color) (background dark))  (:foreground "#999"))
-  (((class color) (background light)) (:foreground "gray75"))
-  (t (:bold t :italic t)))
-  "Todo-List mode face used for completed task."
-  :group 'todo-list-mode-highlighting-faces)
-(defvar todo-list-complete-face 'todo-list-complete-face
-  "Todo-List mode face used for completed task.")
+(defface todo-list-url-face
+  '((t (:foreground "#9C6363")))  ; zenburn-red-3
+  "Face for URLs."
+  :group 'todo-list)
 
-;;
-;; convenience functions
-;;
+(defvar todo-list-font-lock-keywords
+  '(("^@[[:graph:]]+" . 'todo-list-project-face)
+    ("^#+ \\(.+\\)" . 'todo-list-heading-face)
+    ("//.*$" . 'todo-list-comment-face)
+    ("https?://[[:graph:]]+" 0 'todo-list-url-face t))
+  "Syntax highlighting for `todo-list-mode'.")
 
-(defun todo-list-sort-buffer ()
-  "Run sort-lines on entire buffer and remove empty lines."
-  (interactive)
-  (mark-whole-buffer)
-  (flush-lines "^$")
-  (sort-lines nil (region-beginning) (region-end))
-  (delete-trailing-whitespace))
+;; FILE MANAGEMENT
 
-(defun todo-list-cursor-jump ()
-  "Quick cursor jump between formatting columns."
-  (interactive)
-  (if (>= (current-column) 3)
-    (move-beginning-of-line 1)
-    (progn
-      (move-end-of-line 1)
-      (if (< (current-column) 3)
-        (indent-to 3)
+(defun todo-list-open-files ()
+  "Open files from `todo-list-files' in side-by-side windows."
+  (if (not (and (plist-member todo-list-files :left)
+                (plist-member todo-list-files :right)))
+    (message "`todo-list-files' requires both `:left' and `:right' entries.")
+    ;; Parse user settings from the plist
+    (let ((left (expand-file-name (plist-get todo-list-files :left)))
+          (right (expand-file-name (plist-get todo-list-files :right)))
+          (hide-left-p (plist-get todo-list-files :hide-left))
+          (hide-right-p (plist-get todo-list-files :hide-right)))
+      (if (not (and (file-exists-p left) (file-exists-p right)))
+        (message "Unable to read both file entries in `todo-list-files'.")
         (progn
-          (move-beginning-of-line 1)
-          (forward-char 3))))))
+          ;; Open the files in side-by-side windows
+          (delete-other-windows)
+          (find-file left)
+          (todo-list-mode 1)
+          (if hide-left-p
+            (outline-hide-body))
+          (split-window-right)
+          (find-file-other-window right)
+          (todo-list-mode 1)
+          (if hide-right-p
+            (outline-hide-body))
+          (select-window (get-buffer-window (find-file-noselect left))))))))
 
-(define-key todo-list-mode-map (kbd "<return>")
-  '(lambda ()
-     "Sort todo-list buffer at end of line, otherwise insert newline."
-     (interactive)
-     (let ((next-char (char-after)))
-       (if (or (null next-char) (char-equal next-char ?\n))
-         (todo-list-sort-buffer)
-         (newline)))))
+(defvar todo-list-files-open-p nil
+  "Flag to track if the todo-list files are currently open.")
 
-(define-key todo-list-mode-map (kbd "C-<return>") 'newline)
-(define-key todo-list-mode-map (kbd "<tab>") 'todo-list-cursor-jump)
- 
+(defvar todo-list-saved-win-config nil
+  "Variable to store the previous window configuration.")
+
+(defun todo-list-toggle ()
+  "Open and close `todo-list-mode' while preserving window configuration."
+  (interactive)
+  (if todo-list-files-open-p
+    (progn
+      (setq todo-list-files-open-p nil)
+      (when todo-list-saved-win-config
+        (set-window-configuration todo-list-saved-win-config)
+        (setq todo-list-saved-win-config nil)))
+    (progn
+      (setq todo-list-saved-win-config (current-window-configuration))
+      (setq todo-list-files-open-p t)
+      (todo-list-open-files)
+      (todo-list-set-font-size))))
+
+(defun todo-list-get-files ()
+  "Return a list of file paths from `todo-list-files'."
+  (mapcar (lambda (key) (plist-get todo-list-files key))
+    '(:left :right)))
+
+(defun todo-list-save-buffers ()
+  "Save any modified buffers in files from `todo-list-files'."
+  (interactive)
+  (dolist (file (todo-list-get-files))
+    (let ((buffer (find-buffer-visiting file)))
+      (when (and buffer (buffer-modified-p buffer))
+        (with-current-buffer buffer
+          (save-buffer))))))
+
+;; OUTLINE
+
+(defun todo-list-toggle-heading ()
+  "Toggle the visibility of the current `outline-mode' heading."
+  (interactive)
+  (save-excursion
+    (outline-back-to-heading)
+    (if (outline-invisible-p (line-end-position))
+      (outline-show-entry)
+      (outline-hide-entry))))
+
+(defun todo-list-toggle-headings ()
+  "Toggle the visibility of all `outline-mode' headings."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((invisible-heading-p nil))
+      (while (and (outline-next-heading) (not invisible-heading-p))
+        (if (outline-invisible-p (line-end-position))
+          (setq invisible-heading-p t)))
+      (if invisible-heading-p
+        (outline-show-all)
+        (outline-hide-body)))))
+
+;; ACCESSIBILITY
+
+(defun todo-list-set-font-size ()
+  "Set the font size according to `todo-list-font-size'."
+  (dolist (file (todo-list-get-files))
+    (let ((buffer (find-buffer-visiting file)))
+      (when buffer
+        (with-current-buffer buffer
+          (text-scale-set todo-list-font-size))))))
+
+(defun todo-list-increase-font-size ()
+  "Increase the font size."
+  (interactive)
+  (setq todo-list-font-size (1+ todo-list-font-size))
+  (todo-list-set-font-size))
+
+(defun todo-list-decrease-font-size ()
+  "Decrease the font size."
+  (interactive)
+  (setq todo-list-font-size (1- todo-list-font-size))
+  (todo-list-set-font-size))
+
+(defun todo-list-reset-font-size ()
+  "Reset the font size."
+  (interactive)
+  (custom-reevaluate-setting 'todo-list-font-size)
+  (todo-list-set-font-size))
+
+;; KEYBINDINGS
+
+(defvar todo-list-mode-map (make-sparse-keymap))
+(define-key todo-list-mode-map (kbd "C-x C-s") 'todo-list-save-buffers)
+(define-key todo-list-mode-map (kbd "<tab>") 'todo-list-toggle-heading)
+(define-key todo-list-mode-map (kbd "S-<tab>") 'todo-list-toggle-headings)
+;; macos
+(define-key todo-list-mode-map (kbd "s-=") 'todo-list-increase-font-size)
+(define-key todo-list-mode-map (kbd "s--") 'todo-list-decrease-font-size)
+(define-key todo-list-mode-map (kbd "s-0") 'todo-list-reset-font-size)
+
+;; MODE
+
+(define-minor-mode todo-list-mode
+  "Set your goals while keeping up with life."
+  :lighter " todo-list"
+  :keymap todo-list-mode-map
+  (outline-minor-mode 1)
+  (setq-local outline-regexp "^#+.*\\|^@[[:graph:]]+")  ; define headings
+  (font-lock-add-keywords nil todo-list-font-lock-keywords)
+  (font-lock-mode 1))
+
 (provide 'todo-list-mode)
