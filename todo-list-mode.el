646f5d92f726e5ca89b5bc07872278cbc37c5a5e
Billy Lamberta <43051+lamberta@users.noreply.github.com>
2025-03-20 18:06:16 -0700

    Refactor and cleanup

--- todo-list-mode.el
+++ todo-list-mode.el
@@ -8,7 +8,7 @@
 ;; Created: Jan 2009
 ;; Updated: Mar 2025
 ;; Keywords: convenience, outlines, org
-;; Version: 2.0
+;; Version: 2.0.1
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This file is free software; you can redistribute it and/or modify
@@ -59,9 +59,9 @@
 
 ;;; Code:
 
+(require 'cl-lib)
 (require 'org)
 (require 'org-id)
-(require 'cl-lib)
 
 ;;; Customization
 
@@ -91,7 +91,7 @@ A plist with keys :projects and :goals pointing to org files."
     (define-key map (kbd "C-<down>") #'org-next-visible-heading)
     (define-key map (kbd "C-<up>") #'org-previous-visible-heading)
     (define-key map (kbd "C-<right>") #'todo-list-next-section)
-    (define-key map (kbd "C-<left>") #'todo-list-previous-section)
+    (define-key map (kbd "C-<left>") #'todo-list-prev-section)
 
     ;; Font size adjustments for macOS
     (when (eq system-type 'darwin)
@@ -109,104 +109,163 @@ A plist with keys :projects and :goals pointing to org files."
 (defvar todo-list--saved-win-config nil
   "Variable to store the previous window configuration.")
 
-;;; Internal functions
+;;; File management
 
 (defun todo-list--get-files ()
-  "Return a list of file paths from `todo-list-files'.
-Ensures paths are expanded to absolute paths."
-  (let ((projects (plist-get todo-list-files :projects))
-        (goals (plist-get todo-list-files :goals))
-        files)
-    (when projects
-      (push (expand-file-name projects) files))
-    (when goals
-      (push (expand-file-name goals) files))
-    (nreverse files)))
+  "Validate and return the project and goal files.
+Returns project and goal files if valid, signals error otherwise."
+  (unless (and (plist-member todo-list-files :projects)
+               (plist-member todo-list-files :goals))
+    (user-error "`todo-list-files' requires both `:projects' and `:goals' entries"))
 
-(defun todo-list--validate-files ()
-  "Validate that both project and goal files are specified and exist.
-Return non-nil if both files are valid, otherwise show error and return nil."
-  (if (not (and (plist-member todo-list-files :projects)
-                (plist-member todo-list-files :goals)))
-    (user-error "`todo-list-files' requires both `:projects' and `:goals' entries")
-    (let ((projects (expand-file-name (plist-get todo-list-files :projects)))
-          (goals (expand-file-name (plist-get todo-list-files :goals))))
-      (if (not (and (file-exists-p projects) (file-exists-p goals)))
-        (user-error "Unable to read both file entries in `todo-list-files'")
-        t))))
+  (let ((projects (expand-file-name (plist-get todo-list-files :projects)))
+        (goals (expand-file-name (plist-get todo-list-files :goals))))
+    (unless (and (file-exists-p projects) (file-exists-p goals))
+      (user-error "Unable to read both file entries in `todo-list-files'"))
+    (cl-values projects goals)))
 
-(defun todo-list--first-headline-recenter ()
-  "Move to the first headline and recenter.
-If no headline is found, stays at point-min."
-  (goto-char (point-min))
-  (when (re-search-forward org-heading-regexp nil t)
-    (beginning-of-line)
+(defun todo-list--setup-file-window (file &optional other-window)
+  "Set up a window to display FILE with todo-list-mode.
+When OTHER-WINDOW is non-nil, use other window."
+  (if other-window
+    (find-file-other-window file)
+    (find-file file))
+  (org-mode)
+  (todo-list-mode 1)
+  ;; Close all property drawers
+  (org-cycle-hide-drawers 'all)
+  ;; Apply visibility settings from properties
+  (org-cycle-set-visibility-according-to-property)
+  ;; Move to first headline and recenter
+  (when (outline-next-heading)
     (recenter-top-bottom 0)))
+
+(defun todo-list--open-files ()
+  "Open files from `todo-list-files' in side-by-side windows."
+  (cl-multiple-value-bind (projects goals) (todo-list--get-files)
+    ;; Open the files in side-by-side windows
+    (delete-other-windows)
+    (todo-list--setup-file-window projects)
+    (split-window-right)
+    (todo-list--setup-file-window goals t)
+    (select-window (get-buffer-window (find-file-noselect projects)))
+    (todo-list-set-font-size)))
+
+(defun todo-list-save-buffers ()
+  "Save any modified buffers in files from `todo-list-files'."
+  (interactive)
+  (cl-multiple-value-bind (projects goals) (todo-list--get-files)
+    (dolist (file (list projects goals))
+      (let ((buffer (find-buffer-visiting file)))
+        (when (and buffer (buffer-modified-p buffer))
+          (with-current-buffer buffer
+            (save-buffer)))))))
+
+(defun todo-list--close-files ()
+  "Close todo-list files and restore window configuration."
+  (setq todo-list--files-open-p nil)
+  ;; Save and kill the buffers
+  (todo-list-save-buffers)
+  (cl-multiple-value-bind (projects goals) (todo-list--get-files)
+    (dolist (file (list projects goals))
+      (when-let ((buffer (find-buffer-visiting file)))
+        (kill-buffer buffer))))
+  ;; Restore window configuration
+  (when todo-list--saved-win-config
+    (set-window-configuration todo-list--saved-win-config)
+    (setq todo-list--saved-win-config nil)))
 
 (defun todo-list--kill-buffer-hook ()
   "Hook to run when a buffer with todo-list-mode is killed.
 Restores window configuration when the last todo-list buffer is closed."
   (when todo-list--files-open-p
-    (let ((files (todo-list--get-files)))
-      (when (and files
-                 (member (buffer-file-name) files)
-                 (= 1 (length (seq-filter
-                                (lambda (f)
-                                  (get-buffer (file-name-nondirectory f)))
-                                files))))
-        ;; This is the last of our managed files being killed
-        (setq todo-list--files-open-p nil)
-        (when todo-list--saved-win-config
-          (set-window-configuration todo-list--saved-win-config)
-          (setq todo-list--saved-win-config nil))))))
+    (cl-multiple-value-bind (projects goals) (todo-list--get-files)
+      (let ((files (list projects goals)))
+        (when (and (member (buffer-file-name) files)
+                   (= 1 (cl-count-if (lambda (f)
+                                       (get-buffer (file-name-nondirectory f)))
+                          files)))
+          ;; This is the last of our managed files being killed
+          (setq todo-list--files-open-p nil)
+          (when todo-list--saved-win-config
+            (set-window-configuration todo-list--saved-win-config)
+            (setq todo-list--saved-win-config nil)))))))
 
 ;;; Navigation commands
 
 (defun todo-list-next-section ()
   "Move to next top-level section in the current buffer."
   (interactive)
-  (widen)
-  (if (not (org-at-heading-p))
-    ;; Not at heading - find first heading
-    (progn
-      (goto-char (point-min))
-      (unless (re-search-forward "^\\* " nil t)
-        (message "No headings in buffer")))
-    ;; At heading - go to next top-level heading
-    (let ((level (org-outline-level)))
-      (if (= level 1)
-        ;; At level 1 - try to move to next level 1
+  (save-restriction
+    (widen)
+    (cond
+      ;; Not at heading - go to first heading
+      ((not (org-at-heading-p))
+        (goto-char (point-min))
+        (unless (outline-next-heading)
+          (message "No headings in buffer")))
+      ;; At level 1 - move to next level 1
+      ((= 1 (org-outline-level))
         (unless (org-forward-heading-same-level 1)
-          (message "No next top-level heading"))
-        ;; At deeper level - first go up to level 1
-        (org-up-heading-all (1- level))
-        ;; Then try to go forward
+          (message "No next top-level heading")))
+      ;; At deeper level - go up then forward
+      (t
+        (org-up-heading-all (1- (org-outline-level)))
         (unless (org-forward-heading-same-level 1)
           (message "No next top-level heading"))))))
 
-(defun todo-list-previous-section ()
+(defun todo-list-prev-section ()
   "Move to previous top-level section in the current buffer."
   (interactive)
-  (widen)
-  (if (not (org-at-heading-p))
-    ;; Not at heading - find last heading
-    (progn
-      (goto-char (point-max))
-      (unless (re-search-backward "^\\* " nil t)
-        (message "No headings in buffer")))
-    ;; At heading - go to previous top-level heading
-    (let ((level (org-outline-level)))
-      (if (= level 1)
-        ;; At level 1 - try to move to previous level 1
+  (save-restriction
+    (widen)
+    (cond
+      ;; Not at heading - go to last heading
+      ((not (org-at-heading-p))
+        (goto-char (point-max))
+        (unless (re-search-backward "^\\* " nil t)
+          (message "No headings in buffer")))
+      ;; At level 1 - move to previous level 1
+      ((= 1 (org-outline-level))
         (unless (org-backward-heading-same-level 1)
-          (message "No previous top-level heading"))
-        ;; At deeper level - first go up to level 1
-        (org-up-heading-all (1- level))
-        ;; Then try to go backward
+          (message "No previous top-level heading")))
+      ;; At deeper level - go up then backward
+      (t
+        (org-up-heading-all (1- (org-outline-level)))
         (unless (org-backward-heading-same-level 1)
           (message "No previous top-level heading"))))))
 
-;;; File management commands
+;;; Font size commands
+
+(defun todo-list-set-font-size ()
+  "Set the font size according to `todo-list-font-size'.
+Applies to all open todo-list buffers."
+  (cl-multiple-value-bind (projects goals) (todo-list--get-files)
+    (dolist (file (list projects goals))
+      (let ((buffer (find-buffer-visiting file)))
+        (when buffer
+          (with-current-buffer buffer
+            (text-scale-set todo-list-font-size)))))))
+
+(defun todo-list-increase-font-size ()
+  "Increase the font size in all todo-list buffers."
+  (interactive)
+  (setq todo-list-font-size (1+ todo-list-font-size))
+  (todo-list-set-font-size))
+
+(defun todo-list-decrease-font-size ()
+  "Decrease the font size in all todo-list buffers."
+  (interactive)
+  (setq todo-list-font-size (1- todo-list-font-size))
+  (todo-list-set-font-size))
+
+(defun todo-list-reset-font-size ()
+  "Reset the font size to the default value in all todo-list buffers."
+  (interactive)
+  (custom-reevaluate-setting 'todo-list-font-size)
+  (todo-list-set-font-size))
+
+;;; User commands
 
 ;;;###autoload
 (defun todo-list-toggle ()
@@ -214,63 +273,11 @@ Restores window configuration when the last todo-list buffer is closed."
 When closing, Emacs will prompt to save any modified buffers."
   (interactive)
   (if todo-list--files-open-p
+    (todo-list--close-files)
     (progn
-      (setq todo-list--files-open-p nil)
-      ;; Kill the buffers
-      (dolist (file (todo-list--get-files))
-        (when-let ((buffer (find-buffer-visiting file)))
-          (kill-buffer buffer)))
-      ;; Restore window configuration
-      (when todo-list--saved-win-config
-        (set-window-configuration todo-list--saved-win-config)
-        (setq todo-list--saved-win-config nil)))
-    (when (todo-list--validate-files)
       (setq todo-list--saved-win-config (current-window-configuration))
       (setq todo-list--files-open-p t)
-      (todo-list-open-files))))
-
-(defun todo-list-open-files ()
-  "Open files from `todo-list-files' in side-by-side windows."
-  (interactive)
-  (if (not (todo-list--validate-files))
-    (user-error "Invalid todo-list files configuration")
-    (let ((projects (expand-file-name (plist-get todo-list-files :projects)))
-          (goals (expand-file-name (plist-get todo-list-files :goals))))
-      ;; Open the files in side-by-side windows
-      (delete-other-windows)
-      (find-file projects)
-      (org-mode)
-      (todo-list-mode 1)
-      ;; Close all property drawers
-      (org-cycle-hide-drawers 'all)
-      ;; Start cursor at first entry
-      (todo-list--first-headline-recenter)
-
-      ;; Open second file on the right
-      (split-window-right)
-      (find-file-other-window goals)
-      (org-mode)
-      (todo-list-mode 1)
-      ;; Close all property drawers
-      (org-cycle-hide-drawers 'all)
-      (todo-list--first-headline-recenter)
-
-      ;; Return to projects window
-      (select-window (get-buffer-window (find-file-noselect projects)))
-
-      ;; Apply font sizing
-      (todo-list-set-font-size))))
-
-(defun todo-list-save-buffers ()
-  "Save any modified buffers in files from `todo-list-files'."
-  (interactive)
-  (dolist (file (todo-list--get-files))
-    (let ((buffer (find-buffer-visiting file)))
-      (when (and buffer (buffer-modified-p buffer))
-        (with-current-buffer buffer
-          (save-buffer))))))
-
-;;; Task management commands
+      (todo-list--open-files))))
 
 ;;;###autoload
 (defun todo-list-entry-find ()
@@ -281,43 +288,44 @@ Prompts for selection first, then opens the todo-list view if needed."
     (user-error "No todo-list files configured. Set `todo-list-files' first"))
 
   ;; Load files as needed
-  (let ((files (todo-list--get-files))
-        (entries '()))
+  (cl-multiple-value-bind (projects goals) (todo-list--get-files)
+    (let ((files (list projects goals))
+          (entries '()))
 
-    ;; Gather entries from all files - loading if needed
-    (dolist (file files)
-      (with-current-buffer (find-file-noselect file)
-        (org-map-entries
-          (lambda ()
-            (let* ((pos (point))
-                   (file-name (file-name-nondirectory (buffer-file-name)))
-                   (raw-heading (org-get-heading t t t t))
-                   ;; Remove text properties and trim whitespace
-                   (heading (string-trim (substring-no-properties raw-heading)))
-                   (display-name (format "%s | %s"
-                                   heading (file-name-sans-extension file-name))))
-              (push (list display-name file pos) entries)))
-          nil nil)))
+      ;; Gather entries from all files - loading if needed
+      (dolist (file files)
+        (with-current-buffer (find-file-noselect file)
+          (org-map-entries
+            (lambda ()
+              (let* ((pos (point))
+                     (file-name (file-name-nondirectory (buffer-file-name)))
+                     (raw-heading (org-get-heading t t t t))
+                     ;; Remove text properties and trim whitespace
+                     (heading (string-trim (substring-no-properties raw-heading)))
+                     (display-name (format "%s | %s"
+                                     heading (file-name-sans-extension file-name))))
+                (push (list display-name file pos) entries)))
+            nil nil)))
 
-    ;; 1. Select entry first
-    (let* ((choices (mapcar #'car entries))
-           (selection (completing-read "Entry: " choices nil t)))
+      ;; 1. Select entry first
+      (let* ((choices (mapcar #'car entries))
+             (selection (completing-read "Entry: " choices nil t)))
 
-      (if-let ((entry (assoc selection entries)))
-        (let ((target-file (nth 1 entry))
-              (target-pos (nth 2 entry)))
+        (if-let ((entry (assoc selection entries)))
+          (let ((target-file (nth 1 entry))
+                (target-pos (nth 2 entry)))
 
-          ;; 2. Open todo-list view if needed
-          (unless todo-list--files-open-p
-            (todo-list-toggle))
+            ;; 2. Open todo-list view if needed
+            (unless todo-list--files-open-p
+              (todo-list-toggle))
 
-          ;; 3. Navigate to selected entry
-          (let ((buffer (get-file-buffer target-file)))
-            (when buffer
-              (pop-to-buffer buffer)
-              (goto-char target-pos)
-              (org-fold-show-context 'agenda))))
-        (user-error "Entry not found: %s" selection)))))
+            ;; 3. Navigate to selected entry
+            (let ((buffer (get-file-buffer target-file)))
+              (when buffer
+                (pop-to-buffer buffer)
+                (goto-char target-pos)
+                (org-fold-show-context 'agenda))))
+          (user-error "Entry not found: %s" selection))))))
 
 (defun todo-list-entry-add-properties ()
   "Add standard properties to the current heading.
@@ -342,35 +350,6 @@ Requires being in an org buffer."
       (org-entry-put nil "CREATED" (format-time-string "[%Y-%m-%d %a %H:%M]")))
 
     (message "Added properties to current entry")))
-
-;;; Font size commands
-
-(defun todo-list-set-font-size ()
-  "Set the font size according to `todo-list-font-size'.
-Applies to all open todo-list buffers."
-  (dolist (file (todo-list--get-files))
-    (let ((buffer (find-buffer-visiting file)))
-      (when buffer
-        (with-current-buffer buffer
-          (text-scale-set todo-list-font-size))))))
-
-(defun todo-list-increase-font-size ()
-  "Increase the font size in all todo-list buffers."
-  (interactive)
-  (setq todo-list-font-size (1+ todo-list-font-size))
-  (todo-list-set-font-size))
-
-(defun todo-list-decrease-font-size ()
-  "Decrease the font size in all todo-list buffers."
-  (interactive)
-  (setq todo-list-font-size (1- todo-list-font-size))
-  (todo-list-set-font-size))
-
-(defun todo-list-reset-font-size ()
-  "Reset the font size to the default value in all todo-list buffers."
-  (interactive)
-  (custom-reevaluate-setting 'todo-list-font-size)
-  (todo-list-set-font-size))
 
 ;;; Mode definition
 
